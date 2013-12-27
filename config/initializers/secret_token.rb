19e2548d89a413e7770fbc641afe628ef3992ecb
ariroseonline <ariroseonline@gmail.com>
2013-12-27 01:26:57 -0500

    initial commit 3.2.1 blast off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cardigan::Application.config.secret_key_base = '0525687444a779d172881e54b9da3f562fe2a2dfd7ef52380eb484448bf8e525f6a8dd8c2b4a0ef41cad11f65c8dfe114339b5676fa0a8ac9341c239ac0e6503'
